d72a0625e0214aecdfe4d693b59af5229c92f93c
Dupthala Arun Vamshi <83958824+arunvamshi-d@users.noreply.github.com>
2021-09-28 12:09:12 +0530

    Update and rename Pizza.js to Bike.js

--- src/components/Bike.js
+++ src/components/Bike.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Modal } from 'react-bootstrap';
 
-export default function Pizza({pizza}) {
+export default function Bike({bike}) {
     const [Quantity,setquantity]= useState(1)
     const [Varients,setvarients]= useState('small')
     const [show, setShow] = useState(false);
@@ -12,22 +12,22 @@ export default function Pizza({pizza}) {
     return (
         <div style={{margin:'70px'}}className="shadow-lg p-3 mb-5 bg-white rounded">
             <div onClick={handleShow}>
-            <h1>{pizza.name}</h1>
-            <img src={pizza.image} className="img-fluid" style={{heigth:'400px',width:'400px'}}/>
+            <h1>{bike.name}</h1>
+            <img src={bike.image} className="img-fluid" style={{heigth:'200px',width:'200px'}}/>
             </div>
 
             <div className="flex-cointainer">
                 <div className='w-100 m-1'>
-                    <p>TIME IN HRS(Min 6hrs)</p>
+                    <p>TIME(Min 6hrs)</p>
                     <select className='form-control'value={Varients}onChange={(e)=>{setvarients(e.target.value)}}>
-                        {pizza.varients.map(varients=>{
+                        {bike.varients.map(varients=>{
                             return <option value={varients}>{varients}</option>
                         })}
                     </select>
                 </div>
                 <div className='w-100 m-1'>
-                    <p>Distance IN KMS</p>
-                    <select className='form-control'value={Quantity} onChange={(e)=>{setquantity(e.target.value)}}>
+                    <p>DISTANCE IN KMS</p>
+                    <select className='form-control'value={Quantity}onChange={(e)=>{setquantity(e.target.value)}}>
                       {[...Array(50).keys()].map((x,i)=>{
 
                           return <option value={i+20}>{i+20}</option>
@@ -39,19 +39,21 @@ export default function Pizza({pizza}) {
 
             <div className="flex-cointainer">
                 <div className='m-1 w-100'>
-                    <h1 className='mt-1'>Price:{pizza.prices[0][Varients]*Quantity} Rs/-</h1>
+                    <h1 className='mt-1'>Price:{bike.prices[0][Varients]*Quantity} Rs/-</h1>
                 </div>
                 <div className='m-1 w=100'>
-                    <button className="btn">BOOK YOUR RIDE </button>
+                    <button className="btn">BOOK MY RIDE </button>
                 </div>
 
             </div>
             <Modal show={show}>
-    
+  <Modal.Header closeButton>
+    <Modal.Title>{bike.name}</Modal.Title>
+  </Modal.Header>
 
   <Modal.Body>
-    <img src={pizza.image} className="img-fluid" style={{height:'400px'}}/>
-    <p>{pizza.description}</p>
+    <img src={bike.image} className="img-fluid" style={{height:'400px'}}/>
+    <p>{bike.description}</p>
   </Modal.Body>
 
   <Modal.Footer>
